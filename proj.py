1cccabd9cbd133478331b174243a944ccc152070
rubeenferreiraa1 <73401049+rubeenferreiraa1@users.noreply.github.com>
2022-08-09 15:12:57 +0100

    Initial commit

---
+++ proj.py
@@ -0,0 +1,33 @@
+from curses.ascii import isdigit
+import random
+import secrets
+from sysconfig import get_python_version
+
+flag = True
+while flag:
+    num = input("Type a number for an upper bound: ")
+
+    if num.isdigit():
+        print("Lets play!")
+        num = int(num)
+        flag = False
+    else:
+        print("Invalid input! Try again")
+
+secret = random.randint(1, num)
+
+guess = None
+count = 1
+
+while guess != secret:
+    guess = input("Please type a number between 1 and," + str(num) + ": ")
+    if guess.isdigit():
+        guess = int(guess)
+
+    if guess == secret:
+        print("You got it!")
+    else:
+        print("Please try again!")
+        count += 1
+
+print("it took you", count , "guesses")
